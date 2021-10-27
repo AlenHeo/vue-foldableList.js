492be9808b11a78236fd8a00291be49c02c94546
Alen Heo <alenheo@theangelbridge.net>
2021-10-27 21:11:12 +0900

    #1 Project Init

---
+++ src/components/FoldableList.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="list-container">
+        <ul class="list">
+
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'foldable-list',
+};
+</script>
